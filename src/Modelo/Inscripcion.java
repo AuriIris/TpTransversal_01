66a9b0854cb52ade619b497eac1d34cb8a474ba8
MartineBordon26 <martinebordon26@gmail.com>
2021-10-08 19:52:09 -0300

    modelos
    modelos completos

---
+++ src/Modelo/Inscripcion.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author martinbordon
+ */
+public class Inscripcion {
+    
+  private int idInscripcion;
+  private Alumno alumno;
+  private Materia materia;
+  private double nota;
+  private boolean activo;
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota, boolean activo) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+        this.activo = activo;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota, boolean activo) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+        this.activo = activo;
+    }
+
+    public Inscripcion() {
+    }
+
+    public int getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(int idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+    
+    
+}
